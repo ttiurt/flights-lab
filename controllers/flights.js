922924493f38584ef576547b3446543ad0a4db4b
ttiurt <truittzach33@gmail.com>
2023-04-15 18:58:22 -0400

    routes and rollers

---
+++ controllers/flights.js
@@ -0,0 +1,11 @@
+import { Flight } from '../models/flight.js'
+
+function newFlight(req, res){
+  res.render('flights/new', {
+    title: 'Add Flight',
+  })
+}
+
+export {
+  newFlight as new,
+}
--- routes/flights.js
+++ routes/flights.js
@@ -1,10 +1,9 @@
 import { Router } from 'express'
+import * as flightsCtrl from '../controllers/flights.js'
 
 const router = Router()
 
-// GET localhost:3000/users
-router.get('/', function(req, res) {
-  res.send('respond with a resource')
-})
+router.get('/new', flightsCtrl.new)
+
 
 export { router }
